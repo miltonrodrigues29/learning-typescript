9103e8efa9a4082c9ae7333621032b953aa8bd3d
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2022-02-23 15:58:51 +0530

    -

--- main.ts
+++ main.ts
@@ -6,11 +6,30 @@
 
 // log(message);
 
-function doSomething() {
-  for (let x = 0; x < 5; x++) {
-    console.log(x);
-  }
-  console.log(`Finally: ${x}`);
-}
+// ---------------------------
 
-doSomething();
+// function doSomething() {
+//   for (let x = 0; x < 5; x++) {
+//     console.log(x);
+//   }
+//   console.log(`Finally: ${x}`);
+// }
+
+// doSomething();
+
+// let count = 5;
+// count = "a";
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, true, "a", false];
+enum Color {
+  Red,
+  Blue,
+  Orange,
+}
+let bgColor = Color.Blue;
+console.log(bgColor);
